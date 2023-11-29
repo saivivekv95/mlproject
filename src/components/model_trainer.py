b444c51351163a51cacc53231fcd0b8b4b2b74d2
Vivek <saivivekv95@gmail.com>
2023-11-29 20:42:15 +0100

    Hyper parameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,12 +37,51 @@ class ModelTrainer:
                 "Gradient Boosting":GradientBoostingRegressor(),
                 "LinearRegression":LinearRegression(),
                 "K-Neighbours Regression":KNeighborsRegressor(),
-                "XBG Classifier":XGBRegressor(),
-                "Catboost":CatBoostRegressor(),
-                "AdaBoost":AdaBoostRegressor()
+                "XGBRegressor":XGBRegressor(),
+                "CatBoosting Regressor":CatBoostRegressor(),
+                "AdaBoost Regressor":AdaBoostRegressor()
             }
             
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test = X_test,y_test=y_test,models=models)
+            params={
+                "DecisionTree":{
+                    'criterion':['squared_error','poisson','friedman_mse'],
+                    'splitter':['best','random']
+                },
+                "RandomForest":{
+                    'criterion':['poisson','friedman_mse','absolute_error'],
+                    'n_estimators':[10,20,30,40,50,148,290]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression": {},
+                "K-Neighbours Regression":{
+                    'n_neighbors':[5,7,8,9]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test = X_test,y_test=y_test,
+                                              models=models,param=params)
             
             
             best_model_score=max(sorted(model_report.values()))
